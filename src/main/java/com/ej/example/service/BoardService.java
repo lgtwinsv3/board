75b6d31775b609dac5f913d5f3dfe70f27689b79
lgtwinsv3 <eunjung11525@gmail.com>
2017-12-28 10:40:04 +0900

    Generic Service

--- src/main/java/com/ej/example/service/BoardService.java
+++ src/main/java/com/ej/example/service/BoardService.java
@@ -1,52 +1,22 @@
 package com.ej.example.service;
 
+import com.ej.example.dao.DAOSupport;
 import com.ej.example.dao.board.BoardDAO;
 import com.ej.example.domain.BoardDTO;
-import com.ej.example.domain.Paging;
 
 import java.sql.SQLException;
-import java.util.List;
 
-public class BoardService implements IService<BoardDTO> {
-
-    private BoardDAO dao;
-
-    public BoardService() {
-        this.dao = new BoardDAO();
+public class BoardService extends GenericService<BoardDTO, BoardDAO> implements IService<BoardDTO> {
+    public BoardService(DAOSupport dao) {
+        super(dao);
     }
 
-    public Paging<BoardDTO> getList(int page, int rowCount) throws SQLException {
-
-        int count = dao.selectCount();
-
-        Paging<BoardDTO> paging = new Paging<BoardDTO>();
-        paging.calcPaging(count);
-
-        List<BoardDTO> list = dao.selectList(paging.getSkipRowCount(), paging.getRowCount());
-
-        paging.setBody(list);
-
-        return paging;
-    }
-
+    @Override
     public BoardDTO getOne(int seq) throws SQLException {
-        BoardDTO boardDTO = dao.selectOne(seq);
+        BoardDTO boardDTO = getDao().selectOne(seq);
         if (boardDTO != null) {
-            dao.updateReadCount(boardDTO);
+            getDao().updateReadCount(boardDTO);
         }
         return boardDTO;
     }
-
-    public int regist(BoardDTO dto) throws SQLException {
-        return dao.insert(dto);
-    }
-
-    public int modify(BoardDTO dto) throws SQLException {
-        return dao.update(dto);
-    }
-
-    public int remove(int seq) throws SQLException {
-        return dao.delete(seq);
-    }
-
 }
